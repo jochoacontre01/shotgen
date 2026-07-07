9696d3c0ec4723585e9e6db6f8c4c7be93797e01
Jesus Ochoa <>
2026-07-07 08:43:08 -0230

    Initial commit. Auxiliary files during testing or development

---
+++ aux/view_array.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+import subprocess
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-cli", action="store_true")
+args = parser.parse_args()
+
+data = np.load("examples/rtm.npy")
+
+plt.figure(figsize=(8,5))
+plt.imshow(data.T, aspect="auto", cmap="gray")
+plt.colorbar()
+if args.cli:
+    plt.savefig("img.png")
+    subprocess.run("timg img.png".split())
+    subprocess.run("rm img.png".split())
+else:
+    plt.show()
